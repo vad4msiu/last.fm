91951289a52ca00931e4ff91449527cfe3456e25
qqq qqq <vad4msiu@qqqs-MacBook-Pro-2.local>
2012-02-01 19:11:43 +0400

    I don't get paid enough for this shit.

--- run.rb
+++ run.rb
@@ -4,52 +4,74 @@ require "bundler/setup"
 require 'optparse'
 require 'ostruct'
 
-Bundler.require(:default)
+Bundler.require
 
-options = OpenStruct.new
-optparse = OptionParser.new do |opts|
-  opts.banner = "Usage: example.rb [options]"
-
-  opts.on("-f", '--file_name FILE_NAME', "Путь к файлу с артистами (каждый на новой строке)") do |n|
-    options.file_name = n
-  end
-end
-begin
-  optparse.parse!
-  mandatory = [:file_name]
-  missing = mandatory.select{ |param| options.send(param).nil? }
-  if not missing.empty?
-    puts "Missing options: #{missing.join(', ')}"
-    puts optparse
-    exit
-  end
-rescue OptionParser::InvalidOption, OptionParser::MissingArgument
-  puts $!.to_s
-  puts optparse
-  exit
-end
-
-artists = File.read(options.file_name).split("\n")
+# options = OpenStruct.new
+# optparse = OptionParser.new do |opts|
+#   opts.banner = "Usage: example.rb [options]"
+# 
+#   opts.on("-f", '--file_name FILE_NAME', "Путь к файлу с артистами (каждый на новой строке)") do |n|
+#     options.file_name = n
+#   end
+# end
+# begin
+#   optparse.parse!
+#   mandatory = [:file_name]
+#   missing = mandatory.select{ |param| options.send(param).nil? }
+#   if not missing.empty?
+#     puts "Missing options: #{missing.join(', ')}"
+#     puts optparse
+#     exit
+#   end
+# rescue OptionParser::InvalidOption, OptionParser::MissingArgument
+#   puts $!.to_s
+#   puts optparse
+#   exit
+# end
+# 
+# artists = File.read(options.file_name).split("\n")
 LastFM.api_key     = "b25b959554ed76058ac220b7b2e0a026"
 LastFM.client_name = "My app"
 
-# puts LastFM::Artist.get_top_albums(:artist => 'qweqweqwe')['topalbums']['album'].inspect
-artists.each do |artist|
-  response_get_top_albums = LastFM::Artist.get_top_albums(:artist => artist)
-  if response_get_top_albums['error']
-    puts "#{artist}: #{response_get_top_albums['message']}"
+# puts LastFM::Artist.get_top_albums(:artist => 'Chef')['topalbums']['album'].inspect
+
+def check_album(params)
+  params.is_a?(Hash) ? true : false
+end
+
+def check_track(params)
+  params.is_a?(Hash) ? true : false
+end
+
+def check_top_albums(params)
+  return false if params['error']
+  return false unless params['topalbums']
+  return false unless params['topalbums']['album'].is_a?(Array)
+  return true
+end
+
+def check_album_info(params)  
+  if params['error'].nil? && 
+     params['album'] && 
+     params['album']['tracks'] && 
+     params['album']['tracks']['track'].is_a?(Array)
+    true
   else
-    response_get_top_albums['topalbums']['album'].each do |album|
-      response_get_info = LastFM::Album.get_info(:artist => artist, :album => album['name'])
-      if response_get_info['error']
-        puts "#{artist}: #{album['name']}: #{response_get_info['message']}"
-      else
-        if response_get_info['album']['tracks']['track']
-          response_get_info['album']['tracks']['track'].each do |track|
-            puts "#{artist} - #{track['name']}" if track.is_a? Hash
-          end
-        end
-      end
+    false
+  end
+end
+
+artists.each do |artist|
+  top_albums = LastFM::Artist.get_top_albums(:artist => artist)
+  break if check_top_albums(top_albums)
+
+  top_albums['topalbums']['album'].each do |album|
+    break if check_album(album)
+    album_info = LastFM::Album.get_info(:artist => artist, :album => album['name'])
+    break if check_album_info(info)
+    
+    album_info['album']['tracks']['track'].each do |track|
+      puts "#{artist} - #{track['name']}" if track.is_a? Hash
     end
   end
 end
